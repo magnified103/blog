ae6753835051be9c9403c40813c383d0eba07642
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2025-09-11 02:15:34 +0700

    Fix the import script

--- scripts/static_import.py
+++ scripts/static_import.py
@@ -12,13 +12,13 @@ args = parser.parse_args()
 print(f"Importing file from path: {args.file.name}")
 
 if args.ext:
-    ext = args.ext
+    ext = f".{args.ext}"
 else:
     _,ext = os.path.splitext(args.file.name)
 content = args.file.read()
 hash = hashlib.sha256(content).hexdigest()
 print(f"SHA-256 Hash: {hash}")
-dest = f"assets/img/{hash}.{ext}"
+dest = f"assets/img/{hash}{ext}"
 
 with open(dest, "wb") as f:
     f.write(content)
